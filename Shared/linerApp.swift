bdff13abb328569d18ab7157af734e9edf62e0b1
Misha Causur <mishacausur@mail.ru>
2023-02-07 23:24:32 +0500

    Initial Commit

---
+++ Shared/linerApp.swift
@@ -0,0 +1,17 @@
+//
+//  linerApp.swift
+//  Shared
+//
+//  Created by Misha Causur on 07.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct linerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
