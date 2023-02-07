731a167725d91a9f4f8a4a07dfa211bb24a8195d
Misha Causur <mishacausur@mail.ru>
2023-02-07 23:27:43 +0500

    project started

---
+++ Shared/MetalHelper.swift
@@ -0,0 +1,58 @@
+//
+//  MetalHelper.swift
+//  liner
+//
+//  Created by Misha Causur on 07.02.2023.
+//
+
+import SwiftUI
+import MetalKit
+
+struct Metal: View {
+  @State private var metalView = MTKView()
+
+  var body: some View {
+    MetalRepresentable(metalView: $metalView)
+  }
+}
+
+#if os(macOS)
+typealias ViewRepresentable = NSViewRepresentable
+#elseif os(iOS)
+typealias ViewRepresentable = UIViewRepresentable
+#endif
+
+struct MetalRepresentable: ViewRepresentable {
+  @Binding var metalView: MTKView
+
+  #if os(macOS)
+  func makeNSView(context: Context) -> some NSView {
+    metalView
+  }
+    
+  func updateNSView(_ uiView: NSViewType, context: Context) {
+    updateMetalView()
+  }
+    
+  #elseif os(iOS)
+  func makeUIView(context: Context) -> MTKView {
+    metalView
+  }
+
+  func updateUIView(_ uiView: MTKView, context: Context) {
+    updateMetalView()
+  }
+  #endif
+
+  func updateMetalView() {
+  }
+}
+
+struct MetalView_Previews: PreviewProvider {
+  static var previews: some View {
+    VStack {
+      Metal()
+      Text("Metal")
+    }
+  }
+}
